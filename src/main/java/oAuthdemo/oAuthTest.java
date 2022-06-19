8229e97b1976aadd774c4b6a1f938fdac2eb9d4d
venkatshekhar <shekhar.shetty1992@gmail.com>
2022-06-19 14:49:06 +0530

    Saving in GetCourse response in POJO class object GetCourse and nested POJO classes

--- src/main/java/oAuthdemo/oAuthTest.java
+++ src/main/java/oAuthdemo/oAuthTest.java
@@ -2,18 +2,19 @@ package oAuthdemo;
 
 import static io.restassured.RestAssured.given;
 
+import io.restassured.parsing.Parser;
 import io.restassured.path.json.JsonPath;
+import pojo.GetCourse;
 
 public class oAuthTest {
 
 	public static void main(String[] args) {
 
-		
 //		Hit Below url on browser  
 //		https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email&auth_url=https://accounts.google.com/o/oauth2/v2/auth&client_id=692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com&response_type=code&redirect_uri=https://rahulshettyacademy.com/getCourse.php&state=verifyisha
 //		and login with google account and paste url redirect in below String
 
-		String resp = "https://rahulshettyacademy.com/getCourse.php?state=verifyisha&code=4%2F0AX4XfWjy3ecZBkhvvlkvvKEGGsh22ABwyvmBJ_RL2-IjVh3LBheNWh4nHCXhyH0YyNtkMA&scope=email+openid+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&authuser=1&prompt=none";
+		String resp = "https://rahulshettyacademy.com/getCourse.php?state=verifyisha&code=4%2F0AX4XfWiTyMUSpY0LdMxJqi3yQFL9nMZ39xseGYrF5KkY02qVtyIDaaoHmSShPkSrsncJQA&scope=email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+openid&authuser=1&prompt=none";
 
 		String partialcode = resp.split("code=")[1];
 
@@ -22,18 +23,16 @@ public class oAuthTest {
 		System.out.println("Auth code : " + code);
 
 		// First hit auth url to get Access token
-		String accessTokenResponse = given()
-				.urlEncodingEnabled(false)   //default encoding by restassured will be true 
+		String accessTokenResponse = given().urlEncodingEnabled(false) // default encoding by restassured will be true
 				.queryParams("code", code)
 				.queryParams("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
 				.queryParams("client_secret", "erZOWM9g3UtwNRj340YYaK_W")
-				.queryParams("grant_type", "authorization_code")
-				.queryParams("state", "verifyisha")
+				.queryParams("grant_type", "authorization_code").queryParams("state", "verifyisha")
 				.queryParams("session_state", "ff4a89d1f7011eb34eef8cf02ce4353316d9744b..7eb8")
 				.queryParams("redirect_uri", "https://rahulshettyacademy.com/getCourse.php").when()
 //				.log().all()
 				.post("https://www.googleapis.com/oauth2/v4/token").asString();
-		
+
 		System.out.println("Access Token response :" + accessTokenResponse);
 
 		// using JsonPath object fetch the value of accessToken received upper response
@@ -44,10 +43,16 @@ public class oAuthTest {
 		System.out.println("Access TOken :" + accessToken);
 
 		// hit access url by passing accessToken in query params
-		String response = given().queryParam("access_token", accessToken).when().log().all()
-				.get("https://rahulshettyacademy.com/getCourse.php").asString(); // convert raw response into string
+		GetCourse gc = given().queryParam("access_token", accessToken)
+				.expect().defaultParser(Parser.JSON)   // check whether Response is JSON  or we skip if response-type is application/json in response headers
+				.when()
+				.get("https://rahulshettyacademy.com/getCourse.php").as(GetCourse.class);   // convert respoonse as GetCourse object
 
-		System.out.println(response);
+		System.out.println("LinkedIn :"+ gc.getLinkedIn());
+		System.out.println("Instructor :"+gc.getInstructor());
+		System.out.println(gc.getCourses().getApi().get(1).getCourseTitle());
+
+//		System.out.println(response);
 
 	}
 
